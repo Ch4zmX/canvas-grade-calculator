5d70d94d61a88f8d8c7d8c04186fb62733c23197
Sh4d0wb0Y <jaytripathi100@gmail.com>
2023-05-27 15:58:06 -0700

    how tf does canvas api work

--- grades.py
+++ grades.py
@@ -23,16 +23,29 @@ def get_courses():
 
     json = requests.get(CANVAS + "/api/v1/courses",
                        headers={'Authorization': 'Bearer '+API_KEY}).json()
-    courses = {}
-    for course in json:
-        courses[course['id']] = course['name']
-
-    return courses
+    return json
 
 def get_grades(course_id):
     json = requests.get(CANVAS + f"/api/v1/courses/{course_id}/students/submissions", 
                         headers={'Authorization': 'Bearer '+API_KEY}).json()
     return json
+
+def get_assignment(assignment_id, course_id):
+    URL = CANVAS + f"/api/v1/courses/{course_id}/assignments/{assignment_id}"
+
+    json = requests.get(URL,
+                        headers={'Authorization': 'Bearer '+API_KEY}).json()
+    return json
+
+def get_weight(assignment_id, course_id):
+    URL = CANVAS + f"/api/v1/courses/{course_id}/assignments/{assignment_id}"
+    json = requests.get(URL,
+                        headers={'Authorization': 'Bearer '+API_KEY}).json()
+    weight_id = json['assignment_group_id']
+    WEIGHT_URL = CANVAS + f"/api/v1/courses/{course_id}/assignment_groups/{weight_id}"
+    json = requests.get(WEIGHT_URL, 
+                        headers={'Authorization': 'Bearer '+API_KEY}).json()
+    return json['group_weight'] 
 
 if __name__ == '__main__':
 
@@ -44,9 +57,15 @@ if __name__ == '__main__':
         with open(".env", "w") as f:
             os.putenv("TOKEN", API_KEY)
         print("API Key is valid! Continuing...")
+    
+    json = get_courses()
+    
+    courses = {}
+    for course in json:
+        courses[course['id']] = course['name']
 
-    courses = get_courses()
     course_ids = {}
+
     print("Courses Found: ")
     for i, course_id in enumerate(courses.keys()):
         print(f" {i:<5}Class Name: {courses[course_id]:<50}ID: {course_id}")
@@ -54,4 +73,10 @@ if __name__ == '__main__':
     i = int(input("Enter the number corresponding to your class: "))
 
     grades = get_grades(course_id=course_ids[i])
-    print(grades)
+
+    #with open("grades.json", "w") as json_file:
+        #json.dump(grades, json_file)
+    for grade in grades:
+        assignment = get_assignment(grade['assignment_id'], course_ids[i])
+        print(f"{assignment['name']:<75}{grade['score']}/{assignment['points_possible']:<25} Weight: {get_weight(grade['assignment_id'], course_ids[i])}")
+    #print(grades)
